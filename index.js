08cad8dd289d0d3f8f66623802655e959d474d5b
Brian Norlander <bpnorlander@gmail.com>
2018-05-14 17:45:51 +0000

    Added to home.html

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
 app.set('port', (process.env.PORT || 80));
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + "/Views/home.html");
+	res.sendFile(__dirname + "/views/home.html");
+});
+
+app.get('/RayTracer', function(req, res){
+	res.sendFile(__dirname + "/views/home.html");
 });
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+	console.log("Node app is running at localhost:" + app.get('port'));
 });
