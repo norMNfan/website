3c1f09dfa5bf45c90a63473fd1529acf8856d18b
Brian Norlander <bpnorlander@gmail.com>
2018-05-15 14:52:39 +0000

    Added template for box pictures

--- index.js
+++ index.js
@@ -10,7 +10,27 @@ app.get('/', function(req, res) {
 	res.sendFile(__dirname + "/views/home.html");
 });
 
-app.get('/RayTracer', function(req, res){
+app.get('/Projects', function(req, res) {
+	res.sendFile(__dirname + "/views/projects.html");
+});
+
+app.get('/Travel', function(req, res) {
+	res.sendFile(__dirname + "/views/travel.html");
+});
+
+app.get('/Travel/Germany', function(req, res) {
+	res.sendFile(__dirname + "/views/germany.html");
+});
+
+app.get('/Travel/Philippines', function(req, res) {
+	res.sendFile(__dirname + "/views/philippines.html");
+});
+
+app.get('/Books', function(req, res) {
+	res.sendFile(__dirname + "/views/books.html");
+});
+
+app.get('/Projects/RayTracer', function(req, res){
 	res.sendFile(__dirname + "/views/home.html");
 });
 
