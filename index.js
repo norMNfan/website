e8990b3a6ae76cd948339c20a04f47099afe8ea8
Brian Norlander <bpnorlander@gmail.com>
2018-05-16 18:06:30 +0000

    Added project and travel page templates

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 var express = require('express');
+var fs = require('fs');
 var path = require('path');
+var rfs = require('rotating-file-stream');
+var morgan = require('morgan');
+
+// Start NodeJS app
 var app = express();
 
+var logDirectory = path.join(__dirname, 'log');
+ 
+// ensure log directory exists
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+ 
+// create a rotating write stream
+var accessLogStream = rfs('access.log', {
+  interval: '1d', // rotate daily
+  path: logDirectory
+});
+ 
+// setup the logger
+app.use(morgan('combined', {stream: accessLogStream}));
+
+//Set up port connection
 app.set('port', (process.env.PORT || 80));
+
+// Set up root directory path
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(path.join(__dirname, 'public')));
-
-console.log(__dirname);
 
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + "/views/home.html");
@@ -14,6 +34,10 @@ app.get('/', function(req, res) {
 
 app.get('/Projects', function(req, res) {
 	res.sendFile(__dirname + "/views/projects.html");
+});
+
+app.get('/Projects/RayTracer', function(req, res) {
+	res.sendFile(__dirname + "/views/raytracer.html");
 });
 
 app.get('/Travel', function(req, res) {
